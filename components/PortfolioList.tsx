ef067fc772c01727f44a253f65fbada02717afe5
Sean Kerr <sean@seankerr.com>
2021-12-07 21:03:02 +1100

    adjust padding

--- components/PortfolioList.tsx
+++ components/PortfolioList.tsx
@@ -69,7 +69,7 @@ const PortfolioList: React.FC<PortfolioListProps> = function ({ listData }) {
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8 relative">
+      <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-3 lg:gap-y-0 lg:gap-x-8 relative -mt-32 max-w-8xl mx-auto pt-8 md:pt-26 relative z-10 md:pb-12 pb-32 px-4 sm:px-6 lg:px-8">
         <PortFolioListComponent list={listItems} />
       </div>
       {isFetching && (
