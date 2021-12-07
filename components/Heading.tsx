f7738df9b1839a724c873251dad4f8087c2ac2a5
Sean Kerr <sean@seankerr.com>
2021-12-07 21:02:15 +1100

    adjust padding

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -4,7 +4,7 @@ type HeadingProps = {
 
 const Heading: React.FC<HeadingProps> = function ({ children }) {
   return (
-    <div className="relative max-w-7xl mx-auto pt-16 pb-4 px-4 sm:px-6 lg:px-8">
+    <div className="relative max-w-7xl mx-auto pt-28 pb-28 px-4 sm:px-6 lg:px-8 bg-grayer-500">
       {children}
     </div>
   );
