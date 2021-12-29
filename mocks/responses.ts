aedd29fe4842e4e4b969a4d488e73bb64de0ac5d
Sean Kerr <sean@seankerr.com>
2021-12-29 21:31:39 +1100

    add mock folder and test setup

---
+++ mocks/responses.ts
@@ -0,0 +1,15 @@
+export const portfolioErrorMessage = "Oh no! Something bad happened";
+
+export const portfolioHappyPath = [
+  {
+    id: 16,
+    name: "NRMA",
+    story: "This was a cool project",
+    link: "",
+    image: "xxxxxxx.png",
+  },
+];
+
+export const error = {
+  body: { error: { id: "xxxxxx", portfolioErrorMessage } },
+};
