f3ff48fafe27d2ba2f91202cb402f0ef37ba21e1
Sean Kerr <sean@seankerr.com>
2022-12-18 15:21:00 +1100

    fix family image

--- pages/about.tsx
+++ pages/about.tsx
@@ -30,7 +30,7 @@ export default function About() {
       <div className="grid grid-cols-1 gap-y-20 lg:grid-cols-1 lg:gap-y-0 lg:gap-x-8 relative -mt-32 max-w-8xl mx-auto pt-8 md:pt-26 relative z-10 md:pb-12 pb-32 px-4 sm:px-6 lg:px-8 lg:w-9/12">
         <div className="flex flex-row bg-white rounded-2xl shadow-xl mt-8 h-full">
           <div className="flex-1 relative pt-8 px-12 pb-8 md:px-8">
-            <p className="mt-4 ml-3 text-base text-gray-500">
+            <p className="mt-4 ml-3 text-base text-gray-500 mb-4">
               Hey I{"'"}m Sean. That{"'"}s me in the middle with my wife Anna
               and my two sons Julian, Jordan.
             </p>
@@ -39,7 +39,7 @@ export default function About() {
               src="/family.jpeg"
               alt="Family"
               width={330}
-              height={100}
+              height={300}
             />
             <p className="mt-4 ml-3 text-base text-gray-500">
               I{"'"}ve been a developer for over 20 years. I live in Sydney,
