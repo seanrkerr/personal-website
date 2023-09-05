a42bfca1426c1cf1751b3bdead9c9f4796a6c6c7
Sean Kerr <sean@seankerr.com>
2023-09-05 20:56:09 +1000

    feat(chore): rollback the SEO change that didn't work

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,11 @@
-// import { SEO, useSEO } from 'gatsby-plugin-seo';
+import { SEO, useSEO } from 'gatsby-plugin-seo';
 import { graphql } from 'gatsby';
 import Layout from '@/components/Layout';
 import ErrorComp from '@/components/ErrorComp';
 import Helmet from 'react-helmet';
 
 export default function Home({ data }) {
-  // const { siteUrl } = useSEO();
+  const { siteUrl } = useSEO();
   const {
     allPortfolioHome: { edges } = { allPortfolioHome: null, edges: null },
   } = data;
@@ -17,12 +17,6 @@ export default function Home({ data }) {
           lang: `en`,
         }}
       >
-        <title>Home · Sean Kerr</title>
-        <meta
-          name="ahrefs-site-verification"
-          content="dc81e744fc1751c07164efce0964adc4f15ef95df2bc8dd064dca395d2aefa11"
-          data-react-helmet="true"
-        />
         <meta
           name="ahrefs-site-verification"
           content="dc81e744fc1751c07164efce0964adc4f15ef95df2bc8dd064dca395d2aefa11"
@@ -32,51 +26,8 @@ export default function Home({ data }) {
           name="description"
           content="Hi, I'm Sean a full stack developer from Sydney, Australia."
         />
-        <meta
-          property="og:url"
-          content="https://www.seankerr.com/"
-          data-react-helmet="true"
-        />
-        <meta property="og:type" content="website" data-react-helmet="true" />
-        <meta property="og:title" content="Home" data-react-helmet="true" />
-        <meta
-          property="og:description"
-          content="Homepage of Sean Kerr"
-          data-react-helmet="true"
-        />
-        <meta property="og:locale" content="en_US" data-react-helmet="true" />
-        <meta
-          property="og:site_name"
-          content="Sean Kerr"
-          data-react-helmet="true"
-        />
-        <meta
-          property="og:image"
-          content="https://www.seankerr.com/logo.ed3bbad2.png"
-          data-react-helmet="true"
-        />
-        <meta
-          property="og:image:secure_url"
-          content="https://www.seankerr.com/logo.ed3bbad2.png"
-          data-react-helmet="true"
-        />
-        <meta
-          name="twitter:card"
-          content="summary_large_image"
-          data-react-helmet="true"
-        />
-        <meta
-          name="twitter:site"
-          content="@twitterhandle"
-          data-react-helmet="true"
-        />
-        <meta
-          name="twitter:creator"
-          content="@kerrse"
-          data-react-helmet="true"
-        />
       </Helmet>
-      {/* <SEO
+      <SEO
         title="Home"
         description="Homepage of Sean Kerr"
         pagePath="/"
@@ -89,7 +40,7 @@ export default function Home({ data }) {
                 "image": "${siteUrl}/img/logo.ed3bbad2.png"
               }
             }`}
-      /> */}
+      />
 
       <section
         className="-mt-32 max-w-8xl mx-auto pt-16 md:pt-26 relative z-10 md:pb-12 pb-32 px-4 sm:px-6 lg:px-8 lg:w-9/12"
